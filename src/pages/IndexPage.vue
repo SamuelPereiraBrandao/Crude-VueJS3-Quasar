39a93b783e6d77e553e8725d9be1f17a45876178
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-09-12 19:30:33 -0300

    add

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,31 @@
 <template>
   <q-page padding>
+    <q-table
+      title="Treats"
+      :rows="posts"
+      :columns="columns"
+      row-key="name"
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
+import { defineComponent, ref } from 'vue'
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup () {
+    const posts = ref([])
+    const columns = [
+      { name: 'id', field: 'id', label: 'Id', sortable: true, align: 'left' },
+      { name: 'title', field: 'title', label: 'Título', sortable: true, align: 'left' },
+      { name: 'author', field: 'author', label: 'Autor', sortable: true, align: 'left' },
+      { name: 'actions', field: 'actions', label: 'Ações', align: 'right' }
+    ]
+
+    return {
+      posts,
+      columns
+    }
+  }
 })
 </script>
